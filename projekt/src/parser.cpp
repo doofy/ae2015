fc0602b1ff4daca32caffca3510370beecea6f7d
Michael Stypa <michael@stypa.info>
2015-11-10 16:47:53 +0100

    parameter interpretation

--- projekt/src/parser.cpp
+++ projekt/src/parser.cpp
@@ -3,6 +3,48 @@
 
 using namespace std;
 
+void evalInput() {
+}
+
+void calcSolution() {
+}
+
+void writeOutput() {
+}
+
+void readInput() {
+}
+
+int parse(int argc, char** argv) {
+    if (argc == 3) {
+        if (argv[1] == "-eval") {
+            dat1 = argv[2];
+            readInput();
+            evalInput();
+            return 0;
+        }
+        else {
+            return 1;
+        }
+    }
+    else if (argc == 5) {
+        if (argv[1] == "-in" && argv[3] == "-out") {
+            dat1 = argv[2];
+            dat2 = argv[4];
+            readInput();
+            calcSolution();
+            writeOuput();
+            return 0;
+        }
+        else {
+            return 1;
+        }
+    }
+    else {
+        return 1;
+    }
+}
+
 void print_usage(char* progname) {
     cout << "Usage: " << progname << " -in <infile> -out <outfile> | -eval <infile>" << endl;
     cout << "\t-in <infile>: call infile to run algorithms on, need -out" << endl;
@@ -14,10 +56,11 @@ void print_usage(char* progname) {
  *
  */
 int main(int argc, char** argv) {
-    if (argc == 1) {
+    if (parse(argc, argv)) {
         print_usage(argv[0]);
+        return 1;
+    }
+    else {
         return 0;
     }
-
-    return 0;
 }
